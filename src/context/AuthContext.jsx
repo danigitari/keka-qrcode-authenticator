96de7bea36870137929711ddcff23bf73082b74b
DAN <danigitari@gmail.com>
2023-06-13 23:26:36 +0300

    ..

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,11 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword  , onAuthStateChanged ,signInWithEmailAndPassword , signOut } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../firebase";
-
-
 
 const UserContext = createContext();
 
@@ -14,21 +17,22 @@ export const AuthContextProvider = ({ children }) => {
   };
 
   const login = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password); }
-  const logout = () =>  signOut(auth); 
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logout = () => signOut(auth);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth,  (user) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
       console.log(user);
     });
-    return ()=> {
-        unsubscribe();
-    }
+    return () => {
+      unsubscribe();
+    };
   }, []);
 
   return (
-    <UserContext.Provider value={{ createUser , user , logout , login}}>
+    <UserContext.Provider value={{ createUser, user, logout, login }}>
       {children}
     </UserContext.Provider>
   );
